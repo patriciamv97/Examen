3bf5f2244a950ccbe8f196571badf9e3744da7c7
Patricia <pmaceirasvarela@danielcastelao.org>
2021-12-13 10:57:21 +0100

    Ejercicio 2 terminado explicación de los atributos en el layout

---
+++ app/src/main/java/com/dam2a/examen/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dam2a.examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
